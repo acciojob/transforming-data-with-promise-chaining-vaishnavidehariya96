e3355183e2a2203d44682fa529fcb53ff7e45d06
Vaishnavi Dehariya <vaishnavidehariya96@gmail.com>
2025-12-03 10:09:14 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+let calc = {
+    value: 0,
+
+    add(num) {
+        this.value += num;
+        return this; // chaining
+    },
+
+    multiply(num) {
+        this.value *= num;
+        return this; // chaining
+    },
+
+    subtract(num) {
+        this.value -= num;
+        return this; // chaining
+    },
+
+    reset() {
+        this.value = 0;
+        return this; // chaining
+    },
+
+    show() {
+        document.getElementById("result").innerText = this.value;
+        return this; // chaining continue
+    }
+};
